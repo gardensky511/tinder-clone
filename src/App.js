05960016c18b099b2d41def2f939c498f8fecda0
jeongmin <luvmini511@gmail.com>
2020-09-05 19:46:02 +0900

    add buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import TinderCards from "./components/TinderCards";
+import SwipeButtons from "./components/SwipeButtons";
 
 // BrowserRouter : HTML5의 API를 이용하여 페이지를 새로고침 하지 않고도 주소를 변경
 // Switch : 자식 컴포넌트 Route또는 Redirect중 매치되는 첫 번째 요소를 렌더링(사용하지 않을 경우 매칭되는 모두를 렌더링)
@@ -17,10 +18,10 @@ function App() {
           <Route path="/chat">
             <h1>I am the chtapage</h1>
           </Route>
-          <TinderCards />
           {/* default 루트는 항상 제일 마지막에! */}
           <Route path="/">
-            <h1>I am homepage</h1>
+            <TinderCards />
+            <SwipeButtons />
           </Route>
         </Switch>
       </BrowserRouter>
--- src/components/TinderCards/index.js
+++ src/components/TinderCards/index.js
@@ -6,13 +6,22 @@ import database from "../../firebase";
 function TinderCards() {
   const [people, setPeople] = useState([]);
 
+  // useEffect 는 기본적으로 렌더링 되고난 직후마다 실행
+  // 두번째 파라미터 배열에 무엇을 넣느냐에 따라 실행되는 조건이 달라짐
   useEffect(() => {
-    database
+    const unsubscribe = database
       .collection("people")
       // onSnapshot()로 문서를 리슨
       // →콜백이 최초로 호출될 때는 현재 내용으로 문서 스냅샷이 즉시 생성
       // →내용이 변경될 때마다 콜백이 호출되어 문서 스냅샷을 업데이트
       .onSnapshot(snapshot => setPeople(snapshot.docs.map(doc => doc.data())));
+
+    // cleanup함수 (useEffect의 뒷정리)
+    // 외부 데이터 소스 구독 기능을 설정 등의 경우엔 메모리 누수가 발생하지 않도록 뒷정리를 해야함
+    // 컴포넌트가 언마운트되기 전이나, 업데이트 되기 직전에 작업을 수행하고 싶다면 cleanup함수를 반환해야 함
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   return (
